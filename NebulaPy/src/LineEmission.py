fixed bug in the calculation of line luminosity

NebulaPy/src/LineEmission.py
@@ -6,7 +6,7 @@ import NebulaPy.tools.util as util
 class emissionline():
 
     ######################################################################################
-    # get parameter in spherical grid setup
+    # initializing the class emissionline
     ######################################################################################
     def __init__(self, ion, verbose):
         """
@@ -20,22 +20,20 @@ class emissionline():
     # line luminosity
     ######################################################################################
     def lineluminosity_spherical(self, line, temperature, ne, ns, dV):
-        """
-        Calculate the free-free energy loss rate of an ion. The result is returned to the
-        `free_free_loss` attribute.
-        The free-free radiative loss rate is given by Eq. 5.15a of [107]_. Writing the numerical
-        constant in terms of the fine structure constant :math:`\\alpha`,
+        '''
 
-        .. math::
-        \\frac{dW}{dtdV} = \\frac{4\\alpha^3h^2}{3\pi^2m_e}\left(\\frac{2\pi k_B}{3m_e}\\right)^{1/2}Z^2T^{1/2}\\bar{g}_B
+        Parameters
+        ----------
+        line
+        temperature
+        ne
+        ns
+        dV
 
-        where where :math:`Z` is the nuclear charge, :math:`T` is the electron temperature, and
-        math:`\\bar{g}_{B}` is the wavelength-averaged and velocity-averaged Gaunt factor. The
-        Gaunt factor is calculated using the methods of [103]_. Note that this expression for the
-        loss rate is just the integral over wavelength of Eq. 5.14a of [107]_, the free-free emission, and
-        is expressed in units of erg :math:`\mathrm{cm}^3\,\mathrm{s}^{-1}`.
+        Returns
+        -------
 
-        """
+        '''
 
         ion = chianti(ionName=self.Ion, temperature=temperature, ne=ne, verbose=self.Verbose)
 
@@ -74,7 +72,7 @@ class emissionline():
         self.LineLuminosity['dV'] = dV
 
         # Calculating line Luminosity
-        luminosity = np.sum(emissivity * ns * dV)
+        luminosity = 4.0 * const.pi * np.sum(emissivity * ns * dV)
 
         self.LineLuminosity['luminosity'] = luminosity
 