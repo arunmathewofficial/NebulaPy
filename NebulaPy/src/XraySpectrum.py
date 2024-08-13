b01ed78372bc6c755972d4396655739431277fac
Arun Mathew <arun@cp.dias.ie>
2024-08-13 12:44:42 +1000

    updating xray calculation

--- NebulaPy/src/XraySpectrum.py
+++ NebulaPy/src/XraySpectrum.py
@@ -4,10 +4,15 @@
 
 class xray:
 
+    def __init__(self, min_xray_energy, max_xray_energy, elements, verbose=True,):
 
-
-    def __init__(self, min_xray_energy, max_xray_energy, verbose=True, ):
         self.min_energy = min_xray_energy
         self.max_energy = max_xray_energy
+        self.elements = elements
         self.verbose = verbose
+        self.setup()
+
+    def setup(self):
+        print(self.elements)
+
 
--- tests/Xray_emission_measure.py
+++ tests/Xray_emission_measure.py
@@ -1,10 +1,11 @@
 import NebulaPy.src as nebula
 from NebulaPy.tools import util
 
+'''
 # Set up paths and filenames
 silo_dir = '/home/tony/Desktop/NebulaPy/tests/wind-wind-jm'  # Directory containing silo files
 filebase = 'e7_WRwind_d1l5n256_v0750'  # Base name of the silo files
-output_file = '/home/tony/Desktop/NebulaPy/tests/line_luminosity_OIV25.txt'  # Output file for results
+output_file = '/home/tony/Desktop/NebulaPy/tests/xray.txt'  # Output file for results
 
 # Batch the silo files according to the time instant
 batched_silos = util.batch_silos(silo_dir, filebase)
@@ -16,6 +17,9 @@ nebula_pion = nebula.pion(batched_silos, verbose=True)
 # This uses the first time instant's silo file to initialize
 nebula_pion.get_chemistry()
 
-xray_emission = nebula.xray(
+elements = nebula_pion.chemistry_container['tracer_elements']
+'''
 
-)
+elements = ['H', 'He', 'C', 'N', 'O', 'Ne', 'Si']
+
+xray_emission = nebula.xray(0.3, 7.0, elements, verbose=True)
