d0bf4957ba49c82924cce6055fbeb04076b4489e
Arun-Mathew-GitHub <tonykochumalil@gmail.com>
2024-09-09 23:15:33 +1000

    updating

--- problems/atlas_sed.py
+++ problems/atlas_sed.py
@@ -9,8 +9,8 @@ EnergyBins = [[7.902470e+00, 1.126030e+01], [1.126030e+01, 1.359840e+01],
                  [4.788780e+01, 5.441780e+01], [5.441780e+01, 6.342330e+01],
                  [6.342330e+01, 7.700000e+01]]
 
-plot_dir = '/Users/tony/Desktop/NebulaPy/tests'
-pion_formar_path = '/Users/tony/Desktop/NebulaPy/tests'
+plot_dir = '/Users/tony/Desktop/NebulaPy/problems'
+pion_formar_path = '/Users/tony/Desktop/NebulaPy/problems'
 
 atlas_sed = nebula.sed(EnergyBins, verbose=True, plot=plot_dir, pion=pion_formar_path)
 #atlas_sed = nebula.sed(EnergyBins, verbose=True)
