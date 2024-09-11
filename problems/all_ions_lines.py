130f552dc2a7583fbae94f4b470e141271987e4b
Arun-Mathew-GitHub <tonykochumalil@gmail.com>
2024-09-11 10:07:54 +1000

    updating

--- problems/all_ions_lines.py
+++ problems/all_ions_lines.py
@@ -7,7 +7,7 @@ ne = [1e+9]
 
 output_path = '/home/tony/Desktop/Equi_NonEqui/'
 
-nebula_chianti = nebula.chianti(pion_ion='N3+', temperature=temperature, ne=ne, verbose=True)
+nebula_chianti = nebula.chianti(pion_ion='O7+', temperature=temperature, ne=ne, verbose=True)
 
 spectroscopic_name = nebula_chianti.chianti_ion.Spectroscopic
 
