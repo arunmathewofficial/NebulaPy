d46146ac6cfbdc0edd0aacc6434104311d93b10b
Arun Mathew <arun@cp.dias.ie>
2025-12-13 18:33:45 +1100

    minor updates

--- problems/CollidingWindBinaries2026/Stromlo-CWB.py
+++ problems/CollidingWindBinaries2026/Stromlo-CWB.py
@@ -80,7 +80,7 @@ for step, silo_instant in enumerate(batched_silos):
     # rows = hemispheres (upper, lower), cols = ions
     fig, axes = plt.subplots(
         2, len(ion_list),
-        figsize=(3.5, 4.0),
+        figsize=(4.0, 3.8),
         sharex=True,
         sharey=False
     )
