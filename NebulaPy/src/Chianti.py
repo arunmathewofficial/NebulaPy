ff-calculation bug fixed. revisiting DEM calculation

NebulaPy/src/Chianti.py
@@ -160,26 +160,37 @@ class chianti:
 
         # Loop through the sorted keys in the dictionary of species
         if self.verbose:
-            print(f" retrieving species attributes")
+            print(f"Retrieving species attributes")
 
+        # First pass: compute width
+        names_tmp = [
+            chianti_util.convertName(akey)['spectroscopic']
+            for akey in sorted(species.Todo.keys())
+        ]
+        width = max(len(name) for name in names_tmp) + 2  # tight padding
+
+        # Loop with 6 columns
+        ncol = 7
         count = 0
-        for akey in sorted(species.Todo.keys()):
-            self.species_attributes_container[akey] = chianti_util.convertName(akey)  # Convert the key and store it
-            # If verbose mode is enabled, print the spectroscopic name
-            if self.verbose:
-                # Print a comma-separated list of names with up to 10 items per line
-                print(f" {self.species_attributes_container[akey]['spectroscopic']}", end='')
-                count += 1
-                # Print a newline after every 10 items
-                if count % 10 == 0:
-                    print()  # Move to the next line
-                else:
-                    print(", ", end='')  # Continue on the same line
 
+        for akey in sorted(species.Todo.keys()):
+            self.species_attributes_container[akey] = chianti_util.convertName(akey)
+
+            if self.verbose:
+                name = self.species_attributes_container[akey]['spectroscopic']
+                print(f"{name:<{width}}", end='')
+                count += 1
+
+                if count % ncol == 0:
+                    print()
             self.species_attributes_container[akey]['keys'] = species.Todo[akey]  # Store relevant data
-            # Remove unnecessary data from the dictionary
             del self.species_attributes_container[akey]['filename']
             del self.species_attributes_container[akey]['experimental']
+
+        # handle last row
+        if self.verbose and count % ncol != 0:
+            print()
+
 
 
         # Finalize the species attributes dictionary
@@ -383,14 +394,13 @@ class chianti:
     # all below code need to be verified
     ######################################################################################
 
-    # todo- comment by Arun: working on bremsstrahlung emission
 
     ######################################################################################
-    # get bremsstrahlung emission
+    # get bremsstrahlung emission rate info: verified OK
     ######################################################################################
-    def get_bremsstrahlung_emission(self, wavelength):
+    def get_bremsstrahlung_emission_rate(self, wavelength):
         """
-        Calculates the free-free emission (bremsstrahlung) for a single ion using the following formula:
+        Calculates the free-free emission (bremsstrahlung) rate for a single ion using the following formula:
         .. math::
            \\frac{dW}{dtdVd\lambda} = \\frac{c}{3m_e}\\left(\\frac{\\alpha h}{\pi}\\right)^3
            \\left(\\frac{2\pi}{3m_e k_B}\\right)^{1/2}\\frac{Z^2}{\lambda^2 T^{1/2}}
@@ -401,28 +411,17 @@ class chianti:
 
         The free-free emission is calculated in units of
         :math:`\mathrm{erg}\ \mathrm{cm}^3\ \mathrm{s}^{-1}\ \mathrm{\mathring{A}}^{-1}\ \mathrm{str}^{-1}`.
-        If the emission measure is provided, the result will be multiplied by
-        :math:`\mathrm{cm}^{-5}` (for line-of-sight emission measure) or
-        :math:`\mathrm{cm}^{-3}` (for volumetric emission measure).
 
         Parameters:
         ----------
         wavelength : array-like
             The wavelength(s) at which to calculate the emission, in angstroms.
-        elemental_abundance : float
-            The abundance of the element in the plasma.
-        ion_fraction : float
-            The fraction of the element in the ionization state of interest.
-        emission_measure : float
-            The emission measure, which may be line-of-sight or volumetric.
 
         Returns:
         -------
         free_free_emission : numpy.ndarray
-            The calculated free-free emission for the given wavelength(s).
+            The calculated free-free emission rate for the given wavelength(s).
         """
-
-        print(f"freefree: {self.chianti_ion_name}")
 
         # Calculate the ion's nuclear charge (Z)
         Zion = self.chianti_ion.Ion - 1
@@ -433,7 +432,7 @@ class chianti:
 
         # If verbose, print the ion's spectroscopic label
         if self.verbose:
-            print(f' calculating bremsstrahlung emission for {self.chianti_ion.Spectroscopic}')
+            print(f'Computing bremsstrahlung emission rate for {self.chianti_ion.Spectroscopic}')
 
         # Ensure wavelength is treated as an array
         wavelength = np.atleast_1d(wavelength)
@@ -446,12 +445,6 @@ class chianti:
         # Include temperature dependence in the prefactor
         prefactor *= Zion ** 2 / np.sqrt(self.temperature)
 
-        # Apply the elemental abundance and ion fraction to the prefactor
-        #prefactor *= elemental_abundance * ion_fraction
-
-        # Include the emission measure in the prefactor
-        #prefactor *= emission_measure
-
         # Calculate the exponential factor based on temperature and wavelength
         exp_factor = np.exp(-const.planck * (1.e8 * const.light) / const.boltzmann /
                             np.outer(self.temperature, wavelength)) / (wavelength ** 2)
@@ -461,34 +454,14 @@ class chianti:
         gf_sutherland = continuum_gaunt.sutherland_gaunt_factor(wavelength)
         gf = np.where(np.isnan(gf_itoh), gf_sutherland, gf_itoh)
 
-        # Optionally, apply an energy factor if flux is in photons (commented out by default)
-        energy_factor = 1.0
-        # if chdata.Defaults['flux'] == 'photon':
-        #     energy_factor = const.planck * (1.e8 * const.light) / wavelength
-
         # Calculate the final free-free emission and ensure the result is properly shaped
-        bremsstrahlung_emission = (prefactor[:, np.newaxis] * exp_factor * gf / energy_factor).squeeze()
+        bremsstrahlung_emission_rate = (prefactor[:, np.newaxis] * exp_factor * gf).squeeze()
 
         # If verbose, indicate completion
         if self.verbose:
-            print(f' {self.chianti_ion.Spectroscopic} bremsstrahlung emission calculation completed')
+            print(f"[DONE] {self.chianti_ion.Spectroscopic}: bremsstrahlung emission computed")
 
-        # differential volume emission measure
-        #DVEM = self.ne * species_density * shell_volume
-
-        # multiplying
-        #bremsstrahlung = bremsstrahlung_emission * DVEM[:, np.newaxis]
-
-
-        '''
-        continuum_spectrum.freeFree(wavelength, includeAbund=False, includeIoneq=False)
-        bremsstrahlung_emission = continuum_spectrum.FreeFree['intensity']
-        
-        '''
-
-        print(bremsstrahlung_emission)
-
-        return None
+        return bremsstrahlung_emission_rate
 
 
 

NebulaPy/src/EmissionMeasure.py
@@ -1,3 +1,12 @@
+import numpy as np
+import math
+from astropy import units as u
+import time
+
+pi = math.pi
+m_p = 1.6726219e-24  # g
+k = 1.38064852e-16  # erg/K
+mu = 0.61  # mH
 
 
 class emission_measure():
@@ -10,7 +19,7 @@ class emission_measure():
         pass
 
     ######################################################################################
-    # differential emission measure
+    # differential emission measure todo: not verified
     ######################################################################################
     def DEM(self, dem_indices, ne, shellvolume):
         """
@@ -47,7 +56,7 @@ class emission_measure():
         return DEM
 
     ######################################################################################
-    # generate differential emission measure indices
+    # generate differential emission measure indices todo: not verified
     ######################################################################################
     def generate_dem_indices(self, temperature, Tmin, Tmax, Nbins):
         # Calculate the logarithmic width of each bin
@@ -86,4 +95,64 @@ class emission_measure():
 
         # Return the list of indices for further processing or analysis.
         return {'indices': dem_indices, 'Tb': Tb}
+
+
+    ###############################################################################
+    def volume2D(self, xmax, xmin, ngrid):  # Calculates the volume of each cell in the image grid
+        xmax = xmax
+        xmin = xmin
+        ngrid = ngrid
+        # Calculate the size of each cell in the x, y, and z-direction:
+        delta_z = (xmax[0] - xmin[0]) / ngrid[0]
+        delta_R = (xmax[1] - xmin[1]) / ngrid[1]
+        # Create a 2D array of zeros with the same dimensions as ngrid:
+        v = np.zeros((ngrid[1], ngrid[0]))
+        # Loop through each element in the Volume array:
+        for ycells in range(ngrid[1]):
+            rmin = ycells * delta_R
+            rmax = (ycells + 1) * delta_R
+            for xcells in range(ngrid[0]):
+                v[ycells, xcells] = delta_z * np.pi * (rmax ** 2 - rmin ** 2)
+        del delta_z
+        del delta_R
+        del xmax
+        del xmin
+        del ngrid
+        return v
+
+
+    ###############################################################################
+    def dem2D(self, temp_bin, hw):
+        # Function to calculate the differential emission measure of the nebula
+        # See Green et al. (2019) - Bubble Nebula - paper for details.
+        # temp_bin: array of temperature bins in logspace
+        # hw:       half-width of bin
+        D = self.get_2Darray("Density")
+        density = D['data']
+        temp = self.get_2Darray("Temperature")['data']
+        mask = self.get_2Darray("NG_Mask")['data']
+        ngrid = self.ngrid()
+        lim_max = (D['max_extents'] * u.cm).value
+        lim_min = (D['min_extents'] * u.cm).value
+        sim_time = D['sim_time']
+
+        dem_bin_all = np.zeros(len(temp_bin))
+
+        for j in range(len(density)):
+            denj = density[j]
+            tempj = temp[j]
+            maskj = mask[j]
+            volj = self.volume2D(lim_max[j], lim_min[j], ngrid)
+            nei = denj * maskj
+            nei = nei * 1.2 * 0.715 / m_p  # assume 1.2 electrons per H ion
+            # These are the two arrays I need:
+            vol_den = volj * nei * nei
+            log_masktemp = np.log10(tempj * maskj)
+            # If masktemp[i,j,k] is in one bin range w, then add vol_dens[i,j,k] to dem[w]
+            for w in range(len(temp_bin)):
+              pick = np.zeros_like(log_masktemp)
+              pick[(log_masktemp>=(temp_bin[w]-hw))&(log_masktemp<(temp_bin[w]+hw))] = 1
+              dem_bin_all[w] += np.sum(vol_den * pick)
+
+        return {'sim_time': sim_time, 'dem_bin': dem_bin_all}
 

problems/CollidingWindBinaries2026/XraySpectrum.py
@@ -159,12 +159,13 @@ import NebulaPy.src as nebula
 #from NebulaPy.tools import constants as const
 #import pandas as pd
 
+OutDir = '/Users/tony/Desktop/CWBs-NEMOv1'
 
 spectrum = nebula.spectrum(
-    min_wavelength=None,  # Minimum wavelength in Angstroms
-    max_wavelength=None,  # Maximum wavelength in Angstroms
-    min_photon_energy=1.0,  # Minimum photon energy in keV
-    max_photon_energy=10.0,  # Maximum photon energy in keV
+    min_wavelength=1.0,  # Minimum wavelength in Angstroms
+    max_wavelength=100.0,  # Maximum wavelength in Angstroms
+    min_photon_energy=None,  # Minimum photon energy in keV
+    max_photon_energy=None,  # Maximum photon energy in keV
     N_point=4000,
     bremsstrahlung=True,
     freebound=False,
@@ -176,36 +177,48 @@ spectrum = nebula.spectrum(
     verbose=True
 )
 
-elements = ["H", "He", "Fe"]
-elemental_abundances = {"H": 0.70, "He": 0.28, "Fe": 0.02}
+elements = ["H", "He", "O"]
+elemental_abundances = {"H": 0.70, "He": 0.28, "O": 0.02}
 spectrum.build_species_attributes(elements=elements,
                                   elemental_abundances=elemental_abundances)
 
 
-temperature = [2.e+6, 3.e+7, 1.e+8]
+temperature = [3.e+6, 3.e+7, 1.e+8]
 ne = [1.e+9, 1.e+9, 1.e+9]
 
 
 # to calculate for a single cell or set of cells.
-temperature = [2.e+6, 3.e+7, 1.e+8]
-density = [1.e+9, 1.e+9, 1.e+9]
+#temperature = [2.e+6, 3.e+7, 1.e+8]
+#density = [1.e+9, 1.e+9, 1.e+9]
+#ne = [1.e+9, 1.e+9, 1.e+9]
+
+temperature = [3.e+7]
+density = [1.e+9]
 ne = [1.e+9, 1.e+9, 1.e+9]
 
+
+
+
+# info: only looking at free-free emission
+# my calculation
+wvl = spectrum.wavelength
 spectrum.compute_emission(temperature=temperature, density=density, ne=ne)
+emission_my = spectrum.intensity
+
+c = ch.continuum('o_8', temperature=temperature, em=1.e+27,)
+c.freeFree(wvl, includeAbund=False, includeIoneq=False)
+emission_chianti = c.FreeFree['intensity']
 
 
 
-'''
-fe25 = ch.ion('fe_14', temperature=[2.e+6, 3.e+7], eDensity=1.e+9, em=1.e+27)
-wvl = 200 + 0.125 * np.arange(801)
-fe25.spectrum(wavelength=wvl)
-print(fe25.Spectrum['intensity'].shape)
-fe25.spectrumPlot(wvlRange=[264., 275.], integrated=True, top=5)
+
 plt.figure()
-plt.plot(wvl, fe25.Spectrum['integrated'])
+plt.title(f"OVIII Free–Free Emission Spectrum at T = {temperature[0]:.2e} K")
+plt.plot(wvl, emission_my, linewidth=2, color='black', label='NebulaPy Free-Free')
+plt.plot(wvl, emission_chianti, linewidth=2, color='red', label='ChiantiPy Free-Free', linestyle='dotted')
 xy = plt.axis()
-plt.show()
-'''
+outfile = OutDir + "/xray_spectrum.png"
+plt.savefig(outfile)
 
 
 