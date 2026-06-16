7ecfae4c3d84056b10151b0ea68eee3b0235dde0
Arun-Mathew-GitHub <tonykochumalil@gmail.com>
2026-06-16 11:38:07 +1000

    scripting spectrum method 13

--- NebulaPy/src/Spectrum.py
+++ NebulaPy/src/Spectrum.py
@@ -496,9 +496,9 @@ class spectrum:
 
 
             #if species not in ['fe_25', 'fe_26', 'si_14', 'si_13', 's_16']:
-            if species not in ['h_2']:
+            #if species not in ['h_2']:
             #    #utils.nebula_warning(f"{count} Skipping {species} ...")
-                continue
+            #    continue
 
             # show current species name in progress bar
             #species_iterator.set_postfix_str(species)
